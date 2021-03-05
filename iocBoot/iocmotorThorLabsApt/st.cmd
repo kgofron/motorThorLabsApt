e36a80da36e588bce555efb1842378d1b119d41f
K. Gofron <kgofron@comcast.net>
2021-03-05 10:57:00 -0600

    Initial commit for ThorLabs APT KBD101 controller with DDSM100 stage

---
+++ iocBoot/iocmotorThorLabsApt/st.cmd
@@ -0,0 +1,29 @@
+#!../../bin/linux-x86_64/motorThorLabsApt
+
+#- You may have to change motorThorLabsApt to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/motorThorLabsApt.dbd"
+motorThorLabsApt_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadTemplate "db/user.substitutions"
+dbLoadRecords "db/motorThorLabsAptVersion.db", "user=kaz"
+dbLoadRecords "db/dbSubExample.db", "user=kaz"
+
+#- Set this to see messages from mySub
+#var mySubDebug 1
+
+#- Run this to trace the stages of iocInit
+#traceIocInit
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncExample, "user=kaz"
